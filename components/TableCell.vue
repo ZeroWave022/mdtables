65318686ed07c8c6a12e14a21221ff85f0c451bf
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2023-10-25 21:35:58 +0200

    feat: Add TableCell, integration with editor

---
+++ components/TableCell.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+    const props = defineProps<{
+        modelValue: TableCellData;
+        cellId: string;
+    }>();
+
+    const emit = defineEmits(["update:modelValue"]);
+
+    const modal = ref<null | { showModal: () => void }>(null);
+    const newCellData = ref({ ...props.modelValue });
+
+    const emitNewData = () => {
+        emit("update:modelValue", { ...newCellData.value });
+    };
+
+    const resetData = () => {
+        // Resets newCellData back to parent state, so changes that weren't applied do not persist
+        newCellData.value = Object.assign({ ...props.modelValue });
+    };
+
+    const openModal = () => {
+        // This is a UI function provided by daisyUI
+        modal.value?.showModal();
+        resetData();
+    };
+</script>
+
+<template>
+    <button @click="openModal" class="empty:block empty:w-6 empty:h-8 p-1.5">
+        {{ modelValue.content }}
+    </button>
+    <dialog ref="modal" class="modal">
+        <div class="modal-box prose">
+            <h3>Cell settings</h3>
+            <div class="flex gap-3">
+                <button
+                    class="btn btn-sm btn-neutral"
+                    :class="{ 'btn-info': newCellData.bold }"
+                    @click="newCellData.bold = !newCellData.bold"
+                >
+                    Bold
+                </button>
+                <button
+                    class="btn btn-sm btn-neutral"
+                    :class="{ 'btn-info': newCellData.italic }"
+                    @click="newCellData.italic = !newCellData.italic"
+                >
+                    Italic
+                </button>
+            </div>
+            <label class="label label-text">Cell data</label>
+            <input
+                class="textarea textarea-bordered"
+                v-model="newCellData.content"
+            />
+            <div class="modal-action">
+                <form method="dialog" class="flex gap-2">
+                    <button @click="emitNewData" class="btn btn-success">
+                        Apply
+                    </button>
+                    <button class="btn" @click="resetData">Cancel</button>
+                </form>
+            </div>
+        </div>
+    </dialog>
+</template>
--- components/TableEditor.vue
+++ components/TableEditor.vue
@@ -6,16 +6,16 @@
         return {
             content: "",
             bold: false,
-            italic: false
-        }
-    }
+            italic: false,
+        };
+    };
 
-    const table = useState("table", () => [
-        new Array(5).fill(createTableCell()),
-        new Array(5).fill(createTableCell()),
-        new Array(5).fill(createTableCell()),
-        new Array(5).fill(createTableCell()),
-        new Array(5).fill(createTableCell()),
+    const table: Ref<TableCellData[][]> = useState("table", () => [
+        Array.from({ length: 5 }, () => createTableCell()),
+        Array.from({ length: 5 }, () => createTableCell()),
+        Array.from({ length: 5 }, () => createTableCell()),
+        Array.from({ length: 5 }, () => createTableCell()),
+        Array.from({ length: 5 }, () => createTableCell()),
     ]);
 
     const tableLetters = () => {
@@ -25,19 +25,19 @@
             if (i < alphabet.length) {
                 letters.push(alphabet[i]);
             } else {
-                const fullAlphabetsUsed = Math.floor(i/alphabet.length);
+                const fullAlphabetsUsed = Math.floor(i / alphabet.length);
                 const diff = fullAlphabetsUsed * alphabet.length;
 
-                letters.push(alphabet[fullAlphabetsUsed - 1] + alphabet[i-diff]);
+                letters.push(alphabet[fullAlphabetsUsed - 1] + alphabet[i - diff]);
             }
         }
 
         return letters;
-    }
+    };
 
     // Limit columns and rows to the 1-50 range
     watch(columns, (newColumns, oldColumns) => {
-        columns.value = newColumns < 50 ? newColumns < 1 ? 1 : newColumns : 50;
+        columns.value = newColumns < 50 ? (newColumns < 1 ? 1 : newColumns) : 50;
 
         if (oldColumns < newColumns) {
             for (let row of table.value) {
@@ -51,10 +51,10 @@
     });
 
     watch(rows, (newRows, oldRows) => {
-        rows.value = newRows < 50 ? newRows < 1 ? 1 : newRows : 50;
+        rows.value = newRows < 50 ? (newRows < 1 ? 1 : newRows) : 50;
 
         if (oldRows < newRows) {
-            table.value.push(new Array(columns.value).fill(createTableCell()))
+            table.value.push(new Array(columns.value).fill(createTableCell()));
         } else if (newRows < oldRows) {
             table.value.pop();
         }
@@ -66,25 +66,61 @@
         <div class="flex justify-around">
             <div class="flex gap-2 items-center">
                 <label for="columns">Columns: </label>
-                <input id="columns" v-model="columns" class="input input-bordered" name="rows" type="number" min="1" max="100" placeholder="5" required>
+                <input
+                    id="columns"
+                    v-model="columns"
+                    class="input input-bordered"
+                    name="rows"
+                    type="number"
+                    min="1"
+                    max="100"
+                    placeholder="5"
+                    required
+                />
             </div>
             <div class="flex gap-2 items-center">
                 <label for="rows">Rows: </label>
-                <input id="rows" v-model="rows" class="input input-bordered" name="rows" type="number" min="1" max="100" placeholder="5" required>
+                <input
+                    id="rows"
+                    v-model="rows"
+                    class="input input-bordered"
+                    name="rows"
+                    type="number"
+                    min="1"
+                    max="100"
+                    placeholder="5"
+                    required
+                />
             </div>
         </div>
 
         <div class="p-4 mx-auto bg-base-200 rounded-md shadow-md">
             <table class="">
-                <tbody class="[&_td]:border [&_td]:border-neutral [&_td]:p-1.5" v-for="(row, index) in table">
+                <tbody
+                    class="[&_td]:border [&_td]:border-neutral"
+                    v-for="(row, index) in table"
+                >
                     <tr v-if="index == 0">
                         <td class="border-none"></td>
-                        <td v-for="letter in tableLetters()" class="border-none hover:bg-base-100">{{ letter }}</td>
+                        <td
+                            v-for="letter in tableLetters()"
+                            class="border-none hover:bg-base-100 p-1.5 text-center"
+                        >
+                            {{ letter }}
+                        </td>
                     </tr>
                     <tr>
-                        <td class="border-none hover:bg-base-100">{{ index + 1 }}</td>
-                        <td class="hover:bg-base-100" v-for="data in row">
-                            <button>{{ data.content }}</button>
+                        <td class="border-none hover:bg-base-100 p-1.5">
+                            {{ index + 1 }}
+                        </td>
+                        <td
+                            class="hover:bg-base-100"
+                            v-for="(data, colIndex) in row"
+                        >
+                            <TableCell
+                                v-model="table[index][colIndex]"
+                                :cellId="`${index}_${colIndex}`"
+                            />
                         </td>
                     </tr>
                 </tbody>
@@ -100,8 +136,10 @@
                 | Template | Template | Template | Template | Template |
                 | Template | Template | Template | Template | Template |
             </pre>
-    
-            <button class="btn btn-outline btn-primary mx-auto block">Copy to clipboard</button>
+
+            <button class="btn btn-outline btn-primary mx-auto block">
+                Copy to clipboard
+            </button>
         </div>
     </div>
 </template>
