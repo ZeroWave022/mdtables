600a74526722bfb22770bc4de0a5146df10126d9
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2023-10-26 17:57:08 +0200

    feat: Add simple footer

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,6 +1,7 @@
 <template>
-    <footer class="prose text-center h-[20vh] w-full">
-        <h1>Footer</h1>
-        <p>Footer text...</p>
+    <footer class="px-4 py-2 w-full h-12 bg-base-200">
+        <h3>
+            .mdTables is open source on <a class="link" href="https://github.com/ZeroWave022/mdtables" target="_blank" rel="noreferrer">GitHub</a>
+        </h3>
     </footer>
 </template>
