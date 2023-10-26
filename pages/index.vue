9c15c233cb624861df58df5eb7699aef576b45ee
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2023-10-26 22:21:16 +0200

    fix: Use section instead of article

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <article class="prose prose-md text-center">
+    <section class="prose prose-md text-center">
         <h1>Markdown tables made easy</h1>
         <p>Get started by inserting your data in the table below!</p>
-    </article>
+    </section>
     <TableEditor />
 </template>
